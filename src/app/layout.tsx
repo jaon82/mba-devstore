a143803b7d8671d7db593b823f0df5b4a050256c
Jean Nunes <jean.nunes@uzzipay.com>
2024-10-29 23:29:34 -0400

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+    <html lang="pt-br" className={`${inter.variable} antialiased`}>
+      <body>{children}</body>
     </html>
   );
 }
